6281c539d60100c91826f58c110c6c05f7dea9db
andibabett <andi_babett@yahoo.com>
2019-11-29 08:59:45 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/magazindevopsele/MagazinDeVopseleApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.magazindevopsele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazinDeVopseleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazinDeVopseleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttrackit/magazindevopsele/MagazinDeVopseleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.magazindevopsele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazinDeVopseleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
