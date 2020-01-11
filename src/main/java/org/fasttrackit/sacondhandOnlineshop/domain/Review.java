e3def7eb74daf5ea9b61516bea7f6776326ff99c
andibabett <andi_babett@yahoo.com>
2020-01-11 11:06:05 +0200

    added ManyToOne relationship

---
+++ src/main/java/org/fasttrackit/sacondhandOnlineshop/domain/Review.java
@@ -0,0 +1,21 @@
+package org.fasttrackit.sacondhandOnlineshop.domain;
+
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotNull
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produc_id")
+    private Product product;
+}
